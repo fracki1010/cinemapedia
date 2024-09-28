fb85526f19bb2c52b84ee37681e3453749aeb476
Franco Nicolas Galdame <101910482+fracki1010@users.noreply.github.com>
2024-09-27 23:13:24 -0300

    comienzo del local datasource

--- lib/domain/repositories/local_storage_repository.dart
+++ lib/domain/repositories/local_storage_repository.dart
@@ -1,6 +1,6 @@
 import 'package:cinemapedia/domain/entities/movie.dart';
 
-abstract class LocalStorageDatasource {
+abstract class LocalStorageRepository {
   Future<void> toggleFavorite(Movie movie);
 
   Future<bool> isMovieFavorite(int movieId);
